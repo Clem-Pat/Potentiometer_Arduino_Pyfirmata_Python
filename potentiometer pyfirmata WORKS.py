9c0796c50d146c219811536a0d9b9ae8e59221b2
ClemPat <clement.patrizio@outlook.fr>
2020-01-19 19:07:33 +0100

    Update potentiometer pyfirmata WORKS.py

--- potentiometer pyfirmata WORKS.py
+++ potentiometer pyfirmata WORKS.py
@@ -3,28 +3,33 @@
 import pyfirmata
 from tkinter import *
 
-board=pyfirmata.Arduino('COM5')
-it = pyfirmata.util.Iterator(board)
-it.start()
-board.analog[0].enable_reporting()
-
-root = Tk()
-
 def arret():
     global running
     root.destroy()
     running = False
 
-bouton = Button(root, text = 'stop', cursor='hand2', command=arret)
-bouton.pack()
+def main():
+    global running, root
+    
+    board=pyfirmata.Arduino('COM5')
+    it = pyfirmata.util.Iterator(board)
+    it.start()
+    board.analog[0].enable_reporting()
 
-running = True
-while running == True:
-    try: texte.destroy()
-    except: pass
+    root = Tk()
 
-    analogval = (board.analog[0].read())
-    texte = Label(root, text = 'valeur : {}'.format(analogval), fg='black')
-    texte.pack()
+    bouton = Button(root, text = 'stop', cursor='hand2', command=arret)
+    bouton.pack()
 
-    root.update()
+    running = True
+    while running == True:
+        try: texte.destroy()
+        except: pass
+
+        analogval = (board.analog[0].read())
+        texte = Label(root, text = 'valeur : {}'.format(analogval), fg='black')
+        texte.pack()
+
+        root.update()
+        
+main()
